31fbb12a042ea883351f35e3d0bccc4dab6a79c9
kavisha1411 <kvshghdsr@gmail.com>
2025-05-06 23:31:54 -0400

    Added models, serializers, views and test files

--- myproject/api/urls.py
+++ myproject/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import get_users
+from .views import ListingListCreateView, ListingRetrieveUpdateDestroyView, get_users
 
 urlpatterns = [
+    path('listings/', ListingListCreateView.as_view(), name='listings'),
+    path('listings/<uuid:pk>/', ListingRetrieveUpdateDestroyView.as_view(), name='listing-detail'),
     path('users/', get_users, name='get_users'),
 ]
