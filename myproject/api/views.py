8812744f5953a450587ddc649276097c589176d7
kavisha1411 <kvshghdsr@gmail.com>
2025-05-11 12:35:53 -0400

    Added migration files

--- myproject/api/views.py
+++ myproject/api/views.py
@@ -3,16 +3,12 @@ from .models import Listing
 from .serializers import ListingSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .users import DUMMY_USERS  # Import dummy users
+from users.users import DUMMY_USERS
 
 class ListingListCreateView(generics.ListCreateAPIView):
-    queryset = Listing.objects.all()
+    queryset = Listing.objects.all() 
     serializer_class = ListingSerializer
 
 class ListingRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Listing.objects.all()
     serializer_class = ListingSerializer
-
-@api_view(['GET'])
-def get_users(request):
-    return Response(DUMMY_USERS)
--- myproject/listings/views.py
+++ myproject/listings/views.py
@@ -1,7 +1,12 @@
-from rest_framework import viewsets
+from rest_framework import generics
 from .models import Listing
 from .serializers import ListingSerializer
 
-class ListingViewSet(viewsets.ModelViewSet):
+class ListingListCreateView(generics.ListCreateAPIView):
     queryset = Listing.objects.all()
     serializer_class = ListingSerializer
+
+class ListingRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+    lookup_field = 'pk'
