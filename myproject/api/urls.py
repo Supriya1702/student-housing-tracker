abcddbe04a97a670ec58169a391da70bb570584e
Supriya Kumari <supriya@Mac-20711.lan>
2025-05-11 21:17:11 -0400

    modified urls

--- myproject/api/urls.py
+++ myproject/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ListingListCreateView, ListingRetrieveUpdateDestroyView, get_users
+from .views import ListingListCreateView, ListingRetrieveUpdateDestroyView, get_users, get_listings
 
 urlpatterns = [
-    path('listings/', ListingListCreateView.as_view(), name='listings'),
-    path('listings/<uuid:pk>/', ListingRetrieveUpdateDestroyView.as_view(), name='listing-detail'),
-    path('users/', get_users, name='get_users'),
+    path('listings/', ListingListCreateView.as_view(), name='listing-list-create'),
+    path('listings/<uuid:pk>/', ListingRetrieveUpdateDestroyView.as_view(), name='listing-retrieve-update-destroy'),
+    path('users/', get_users, name='get-users'),
+    path('listings/dummy/', get_listings, name='get-dummy-listings'),
 ]
